d32dc56113bb42d11732ed3f740d9a792318f7a1
Thomas Porro <44237521+thomasporro@users.noreply.github.com>
2020-06-06 11:56:07 +0200

    First commit of the project

---
+++ node_generator.py
@@ -0,0 +1,24 @@
+from xml.dom import minidom
+thiene = minidom.parse('data/Thiene.osm')
+useful_nodes = minidom.parse('data/useful_nodes.osm')
+
+#Extract the way and the nodes from the suorce files
+waylist = thiene.getElementsByTagName('way')
+selected_nodes = useful_nodes.getElementsByTagName('nodes')
+
+#Opens the document where will be written the data
+osm_file = open('data/final_file.osm', 'w')
+osm_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+"\n")
+osm_file.write("<osm version=\"0.6\">"+"\n")
+
+print(waylist[0].nodeName())
+#Writes all the relevant nodes to the file
+#for node in selected_nodes:
+ #   osm_file.write(node.toxml()+"\n")
+
+#for way in waylist:
+    
+
+
+osm_file.write("</osm>")
+osm_file.close()
---
+++ useful_node_exctractor.py
@@ -0,0 +1,66 @@
+from xml.dom import minidom
+
+#Import the file
+doc = minidom.parse('data\Thiene.osm')
+
+#Extract the nodes
+nodelist = doc.getElementsByTagName('node')
+
+#For each node create a new attribute to count how many
+#time that node is inside a way
+for node in nodelist:
+    new_attribute = doc.createAttribute("arity")
+    new_attribute.value = "0"
+    node.setAttributeNode(new_attribute)
+
+#Extract the ways
+waylist = doc.getElementsByTagName('way')
+
+#Check all the nodes of all ways
+for way in waylist:
+    child_nodes = way.getElementsByTagName('nd')
+    
+    #Counter used to check if the node is at the start
+    #or at the end of the way
+    length_counter = 0
+    
+    #For each node inside a way it increases the value of arity
+    for node in child_nodes:
+        for parent in nodelist:
+            if(parent.getAttribute('id')==node.getAttribute('ref')):
+                #Retrieve the value of arity and convert it into an int value,
+                #than it upgrades it
+                value_string = parent.getAttribute('arity')
+                value = int(value_string)
+                value += 1
+
+                #If a node is at the end or at the start of a way, it increases its
+                #value, so we can keep also the closed alleys
+                if(length_counter == 0 or length_counter == (len(child_nodes)-1)):
+                    value += 1
+                
+                #Sets the new value of the arity attribute
+                parent.setAttribute("arity", str(value))
+                
+        length_counter += 1
+
+
+#Opens the document where will be written the data
+osm_file = open('data/final_file.osm', 'w')
+osm_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+"\n")
+osm_file.write("<osm version=\"0.6\">"+"\n")
+
+#If a node have a arity attribute greater than 1 is copied into
+#the file
+for node in nodelist:
+    #We skip this node beceause it causes problem but we don't know why.
+    #Anyway is an useless node
+    if(node.getAttribute("id")=="7558462927"): continue
+
+    #Prints the node inside the file
+    value = int(node.getAttribute("arity"))
+    if(value>1): osm_file.write(node.toxml()+"\n")
+
+#Close the file
+osm_file.write("</osm>")
+osm_file.close()      
